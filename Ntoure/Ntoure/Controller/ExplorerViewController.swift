079ae9a75cda6c781e45825cd64d3a503cef97b1
vBryann <vitorbryan_melo@hotmail.com>
2020-06-13 15:25:10 -0300

    Add SearchController

--- Ntoure/Ntoure/Controller/ExplorerViewController.swift
+++ Ntoure/Ntoure/Controller/ExplorerViewController.swift
@@ -12,17 +12,16 @@ struct CustomData {
     var title: String
     var distance: String
     var image: UIImage
+    var state: String?
 }
 
 class ExplorerViewController: UIViewController {
     
+    //places example
     let data = [
         CustomData(title: "Canoa Quebrada", distance: "136 Km", image: #imageLiteral(resourceName: "canoa")),
-        CustomData(title: "Parque do Cocó", distance: "13 Km", image: #imageLiteral(resourceName: "Parque"))
+        CustomData(title: "Parque do Cocó", distance: "13 Km", image: #imageLiteral(resourceName: "Parque") )
     ]
-    
-    
-    
     
     fileprivate let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout ()
@@ -33,19 +32,45 @@ class ExplorerViewController: UIViewController {
         return cv
     }()
     
+//    fileprivate let location: UILabel = {
+//       let text = UILabel()
+//        text.text = "Ceará, Brasil"
+//        text.textColor = UIColor.black
+//        text.translatesAutoresizingMaskIntoConstraints = false
+//        text.textAlignment = .center
+//        text.font = UIFont.boldSystemFont(ofSize: 18)
+//        return text
+//    }()
+    
+    fileprivate let searchBar: UISearchController = {
+        let searchBar = UISearchController(searchResultsController: nil)
+        searchBar.searchBar.placeholder = "Try Canoa Quebrada"
+        searchBar.searchBar.searchBarStyle = .minimal
+        searchBar.definesPresentationContext = true
+        return searchBar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Explorar"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchBar //SearchController here
+        
         
         view.backgroundColor = .white
         view.addSubview(collectionView)
+        //view.addSubview(location)
+        
         collectionView.backgroundColor = .white
         
         collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
+//        location.topAnchor.constraint(equalTo: view.topAnchor, constant: 140).isActive = true
+//        location.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+
         
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -77,12 +102,13 @@ class CustomCell: UICollectionViewCell {
             guard let data = data else {return}
             bg.image = data.image
             title.text = data.title
+            describ.text = data.distance
         }
     }
     
-    
     fileprivate let bg: UIImageView = {
         let image = UIImageView()
+        
         image.translatesAutoresizingMaskIntoConstraints = false
         image.contentMode = .scaleToFill
         image.clipsToBounds = true
@@ -90,9 +116,19 @@ class CustomCell: UICollectionViewCell {
         return image
         
     }()
+    fileprivate let gradient: UIImageView = {
+        let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "Rectangle 90")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleToFill
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 12
+        return image
+    }()
+    
     fileprivate let title: UILabel = {
         let text = UILabel()
-
+        
         text.textColor = UIColor.white
         text.translatesAutoresizingMaskIntoConstraints = false
         text.textAlignment = .center
@@ -100,21 +136,54 @@ class CustomCell: UICollectionViewCell {
         return text
     }()
     
+    fileprivate let describ: UILabel = {
+        let text = UILabel()
+
+        text.textColor = UIColor.white
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.textAlignment = .center
+        text.font = UIFont.boldSystemFont(ofSize: 15)
+        return text
+    }()
+    
+    fileprivate let icon: UIImageView = {
+       let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "Icon ionic-ios-pin")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    
     override init(frame: CGRect){
         super.init(frame: frame)
         
-        
         contentView.addSubview(bg)
+        contentView.addSubview(gradient)
         contentView.addSubview(title)
+        contentView.addSubview(icon)
+        contentView.addSubview(describ)
+        
 
         bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
         bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
         bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
         
+        gradient.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        gradient.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+        gradient.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
+        gradient.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
         title.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
         title.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 25).isActive = true
         
+        icon.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -45).isActive = true
+        icon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 25).isActive = true
+        icon.widthAnchor.constraint(equalToConstant: 7).isActive = true
+        icon.heightAnchor.constraint(equalToConstant: 10).isActive = true
+        
+        describ.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -41).isActive = true
+        describ.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 35).isActive = true
         
     }
     
